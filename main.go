0ed53709ea424fcaef40465b9cbb988e639dea05
Cody Baker <cbaker@digitalocean.com>
2020-10-28 12:59:44 -0400

    print method and request headers

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+  "fmt"
   "log"
   "net/http"
 )
@@ -10,7 +11,13 @@ func main() {
     Addr: ":8080",
     Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
       w.Header().Add("X-Total-Count", "123")
-      w.WriteHeader(http.StatusNoContent)
+      w.WriteHeader(http.StatusOK)
+      w.Write([]byte(r.Method+"\n\n"))
+      for k, vals := range r.Header {
+        for _, v := range vals {
+          fmt.Fprintf(w, "%s: %s\n", k, v)
+        }
+      }
     }),
   }
   log.Fatal(s.ListenAndServe())
